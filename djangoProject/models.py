affcbfd63911971514d220fd99f8054bf3a50fac
Mus1shi <tomvla49@gmail.com>
2024-10-21 18:19:16 +0200

    connection du back au front, interface admin et menu

--- djangoProject/models.py
+++ djangoProject/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+
     def __str__(self):
         return self.name
 
@@ -20,9 +21,9 @@ class Chef(models.Model):
 class Menu(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.IntegerField()
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    chef = models.ForeignKey('Chef', on_delete=models.CASCADE)
+    chef = models.ForeignKey('Chef', on_delete=models.SET_NULL, null=True, blank=True)
+    price = models.IntegerField(default=0.00)
 
     def __str__(self):
         return self.name
--- djangoProject/views.py
+++ djangoProject/views.py
@@ -1,15 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from djangoProject.models import Category, Menu  # Importer les modèles Category et Menu
 
 def home(request):
     return render(request, 'Menu/home.html')  # Chemin vers home.html dans le dossier Menu
+
 def menu(request):
-    plats = Menu.objects.all()
-    return render(request, 'Menu/menu.html', {'plats': plats})
+    # Récupérer les catégories
+    premiere_selection = get_object_or_404(Category, name='Première Sélection')
+    deuxieme_selection = get_object_or_404(Category, name='Deuxième Sélection')
+    troisieme_selection = get_object_or_404(Category, name='Troisième Sélection')
+
+    # Récupérer les plats par catégorie
+    plats_premiere_selection = Menu.objects.filter(category=premiere_selection)
+    plats_deuxieme_selection = Menu.objects.filter(category=deuxieme_selection)
+    plats_troisieme_selection = Menu.objects.filter(category=troisieme_selection)
+
+    # Passer les plats et les prix des sous-catégories au template
+    context = {
+        'plats_premiere_selection': plats_premiere_selection,
+        'plats_deuxieme_selection': plats_deuxieme_selection,
+        'plats_troisieme_selection': plats_troisieme_selection,
+    }
+
+    return render(request, 'Menu/menu.html', context)
+
 def contact(request):
     return render(request, 'Menu/contact.html')
+
 def history(request):
     return render(request, 'Menu/history.html')
+
 def reservations(request):
     return render(request, 'Menu/reservations.html')
+
 def send_contact(request):
     return render(request, 'Menu/send_contact')
